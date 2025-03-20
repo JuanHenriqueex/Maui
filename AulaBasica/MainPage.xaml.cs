17dac6a520009301318ee78ebb910a8181c53ecb
CORE <CORE@DESKTOP-CORE>
2025-03-20 14:36:13 -0300

    Adicionar arquivos de projeto.

---
+++ AulaBasica/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+namespace AulaBasica
+{
+    public partial class MainPage : TabbedPage
+    {
+        string nome;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void BTNOla_Clicked(object sender, EventArgs e)
+        {
+            nome = await DisplayPromptAsync("Nome", "Digite seu nome: ", "Ok");
+            await DisplayAlert("Aviso", "Olá " + nome, "Ok");
+        }
+    }
+}
