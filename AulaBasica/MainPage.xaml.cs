433fb0d691e13d231ddb2507c42620ccd38e3d87
CORE <henriquejuan009@gmail.com>
2025-03-24 14:46:18 -0300

    :D

--- AulaBasica/MainPage.xaml.cs
+++ AulaBasica/MainPage.xaml.cs
@@ -7,6 +7,12 @@ namespace AulaBasica
         public MainPage()
         {
             InitializeComponent();
+
+            string endereco = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/x9jmkLaIiiE?si=cc7Hfxh4hw-M0fWn\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>";
+            WVVideo.Source = new HtmlWebViewSource
+            {
+                Html = endereco
+            };
         }
         private async void BTNOla_Clicked(object sender, EventArgs e)
         {
